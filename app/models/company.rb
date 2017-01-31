9d8a43c09b66abebfb8b2ac0e0bd216b974e4696
dankim93 <dan_kim93@hotmail.com>
2017-01-31 11:01:53 -0800

    Add logic to update Company info using the api

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,17 @@
 class Company < ApplicationRecord
   validates :name, :price, presence: true
 
+  after_initialize :update_company
+
   has_many :stocks
   has_many :transaction
+
+  def update_company
+    info = StockQuote::Stock.quote(self.symbol)
+    self.name = info.name
+    ask = info.ask
+    bid = info.bid
+    self.price = (ask + bid).to_f/2
+    self.price_percent_change = info.changein_percent
+  end
 end
