967ef0a44587e7ebbb2845c327ef95e00548ddc0
dankim93 <dan_kim93@hotmail.com>
2017-01-31 12:22:09 -0800

    Change model using previous close for price instead of avg of ask and bid

--- app/models/company.rb
+++ app/models/company.rb
@@ -4,14 +4,19 @@ class Company < ApplicationRecord
   after_initialize :update_company
 
   has_many :stocks
-  has_many :transaction
+  has_many :transactions
 
   def update_company
     info = StockQuote::Stock.quote(self.symbol)
     self.name = info.name
-    ask = info.ask
-    bid = info.bid
-    self.price = (ask + bid).to_f/2
-    self.price_percent_change = info.changein_percent
+    # ask = info.ask
+    # bid = info.bid
+    # self.price = (ask + bid).to_f/2
+    self.price = info.previous_close
+    if info.changein_percent
+      self.price_percent_change = info.changein_percent
+    else
+      self.price_percent_change = 0
+    end
   end
 end
