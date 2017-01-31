88fd2f8ffcbd041d5e150929d2a00cd50e5d62fb
Igor Dmitriev <igor.s.dmitriev@gmail.com>
2017-01-30 22:35:24 -0800

    Add access token validation for FB Login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,26 @@
 class User < ApplicationRecord
+  validates :full_name, presence: true
 
+  # before_save :fetch_full_name
 
   def self.find_for_facebook_auth(fb_auth)
-    
-    user = User.where(fb_id: fb_auth['userID']).first_or_create do |u|
+
+    user = User.where(fb_id: fb_auth['userID']).first_or_initialize do |u|
       u.fb_id = fb_auth['userID']
     end
-
-    # TODO try to fetch full_name from fb
-
     user.fb_access_token = fb_auth['accessToken']
+
+    # test for wrong accesstoken
+    # user.fb_access_token = '123123'
+
+    begin
+      graph = Koala::Facebook::API.new(user.fb_access_token)
+      profile = graph.get_object("me")
+      user.full_name = profile['name']
+    rescue
+      user.full_name = nil
+    end
+    debugger
     user
   end
 end
