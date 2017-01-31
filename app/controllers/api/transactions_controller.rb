e1a997da6daac48957c47d134f40630d068a8a9d
Joe Thomas <Joe@Joes-MacBook-Pro.local>
2017-01-31 10:17:24 -0800

    built transaction controller create method

--- app/controllers/api/transactions_controller.rb
+++ app/controllers/api/transactions_controller.rb
@@ -9,17 +9,24 @@ class Api::TransactionsController < ApplicationController
 
   def create
     @user = User.find(params[:user_id])
-    unless @user && @user.money >= params[:price] * params[:stock_count]
-      return json: ['Not enough money'], status: 400
-    end
-    
     @transaction = Transaction.new(transaction_params)
-    if @transaction.save
-      # update or create entry in stock table
+    @stock = Stock.find_or_create_by(user_id: transaction_params[:user_id],
+                                     company_id: transaction_params[:company_id])
+    @stock.shares ||= 0
 
+    if !@user
+      return json: ['Not logged in'], status: 401
+    elsif @user.money < @transaction.price * @transaction.stock_count
+      return json: ['Not enough money'], status: 400
+    elsif @stock.shares + @transaction.stock_count < 0
+      return json: ['Cannot sell more shares than owned'], status: 400
     else
-      # return error
-
+      @transaction.save
+      @stock.update_attributes(transaction.params)
+      @user.update_attributes(money: - @transaction.price *
+                                       @transaction.stock_count)
+      # TODO: write the view for stock
+      return status: 200
     end
   end
 
